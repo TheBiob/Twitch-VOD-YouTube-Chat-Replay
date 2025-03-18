6c506880c6d8d611bc638a863835a3ff8262b15f
TheBiob <tbob862@gmail.com>
2025-03-18 19:21:57 +0100

    seperate download and process scripts

---
+++ scripts/download_and_map_chat.cmd
@@ -0,0 +1,11 @@
+@echo off
+:again
+set /p twitch_id=Twitch Video Id: 
+set /p youtube_id=Youtube Video Id: 
+set RAW_DIRECTORY=../../chat_repository/raw
+
+REM echo .\TwitchDownloaderCLI.exe chatdownload --id "%twitch_id%" --embed-images true --output "%RAW_DIRECTORY%/%youtube_id%.json" --collision Rename
+.\TwitchDownloaderCLI.exe chatdownload --id "%twitch_id%" --embed-images true --output "%RAW_DIRECTORY%/%youtube_id%.json" --collision Rename
+node process_chat_repository.js --files "%youtube_id%"
+set /p other=More? (y/n): 
+if "%other%"=="y" goto again
